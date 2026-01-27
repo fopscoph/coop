e177b427dfd6c3c1f9e4093899ca252535b8321c
Sam <hello@darewp.com>
2026-01-27 12:04:23 +0800

    Content update

---
+++ app/welcome-to-fopsco/page.tsx
@@ -0,0 +1,10 @@
+export default function Welcome() {
+    return (
+        <main className="w-full h-[60vh] bg-ghost-white flex items-center">
+            <div className="mx-auto xl:w-4/5 py-20 px-5 relative">
+                <h1 className="text-2xl font-bold cloud-burst mb-5">Thank you for joining FOPSCo!</h1>
+                <p className="text-lg cloud-burst">A copy of your Share Capital Subscription Agreement will be sent to your inbox shortly.</p>
+            </div>
+        </main>
+    );
+}
